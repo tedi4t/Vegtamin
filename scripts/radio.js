fad7b54ff3df265e008a5d2be8071e815851130f
dmytro <tedi4t@gmail.com>
2021-04-22 17:01:54 +0300

    add date-2 section to plan-date page

--- scripts/radio.js
+++ scripts/radio.js
@@ -1,2 +1,7 @@
-console.log('radio');
-
+$('.radio-item').click(function() {
+  const radioGroup = $(this).closest('.radio-group').first();
+  $(radioGroup).children('.radio-item').removeClass('active');
+  $(this).addClass('active');
+  $(radioGroup).find('.radio-item input[type="radio"]').attr('checked', false);
+  $(this).children('input[type="radio"]').attr('checked', true);
+})
