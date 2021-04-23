fd70746d51b6831619c4842511f717b9d95b523b
dmytro <tedi4t@gmail.com>
2021-04-23 22:18:09 +0300

    add profile-order-plans-details-1 page

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,13 +1,20 @@
 $('.dropdown-toggle').click(function() {
   const dropdownGroup = $(this).closest('.dropdown');
   const dropdownItems = $(dropdownGroup).children('.dropdown-items');
+  console.log(dropdownItems);
   const hidden = dropdownItems.hasClass('hidden');
 
+  console.log('click');
+  console.log(dropdownItems);
+  console.log(hidden);
+
   if (hidden) {
-    dropdownItems.hide(500);
-    dropdownItems.removeClass('hidden');
+    dropdownItems.css('display', 'block');
+    $(this).children('img.dropdown-icon').attr('src', '../../assets/icons/tick-down-green.svg')
+    dropdownItems.removeClass('hidden')
   } else {
-    dropdownItems.show(500);
-    dropdownItems.addClass('hidden');
+    dropdownItems.css('display', 'none');
+    $(this).children('img.dropdown-icon').attr('src', '../../assets/icons/tick-up-green.svg')
+    dropdownItems.addClass('hidden')
   }
 })
