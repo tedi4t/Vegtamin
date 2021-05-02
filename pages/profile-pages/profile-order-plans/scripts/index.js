cea8a70ae9d056a902d16dcdc397eef090a51970
dmytro <tedi4t@gmail.com>
2021-05-02 11:07:52 +0300

    changes to profile pages

--- pages/profile-pages/profile-order-plans/scripts/index.js
+++ pages/profile-pages/profile-order-plans/scripts/index.js
@@ -1,4 +1,5 @@
-$('section#orders-1 button#state-toggler').click(function() {
+$('section#orders-1 button#state-toggler').click(function(e) {
+  e.preventDefault();
   $(this)
     .parent()
     .parent()
