58fd838424d9642c83dbfdc6c6e16985ffcf9f80
dmytro <tedi4t@gmail.com>
2021-05-02 18:22:06 +0300

    added sliders to homepage goods

--- pages/homepage/scripts/index.js
+++ pages/homepage/scripts/index.js
@@ -1,3 +1,5 @@
+let slideOffsetX;
+
 $('#selectors .selector').click(function() {
   $('#selectors .selector').removeClass('active');
   $(this).addClass('active');
@@ -10,4 +12,43 @@ $('#selectors .selector').click(function() {
   $(`section#${forSection}`).css({
     "display": "block"
   })
+})
+
+$('.left-tick-carousel').click(function() {
+  const goodsBlock = $(this)
+    .parent()
+    .children('.goods-block');
+
+  const translateX = goodsBlock.css('transform');
+  const transformMatrix = translateX.split(', ');
+  const currentOffsetX = parseInt(transformMatrix[4]);
+  if (currentOffsetX <= 0 && currentOffsetX > -998) {
+    if (slideOffsetX === undefined) {
+      slideOffsetX = Math.abs(currentOffsetX);
+    }
+    transformMatrix[4] -= slideOffsetX;
+    console.log(transformMatrix.join(', '))
+  
+    goodsBlock.css('transform', transformMatrix.join(', '));    
+  }
+})
+
+$('.right-tick-carousel').click(function() {
+  const goodsBlock = $(this)
+    .parent()
+    .children('.goods-block');
+
+  const translateX = goodsBlock.css('transform');
+  const transformMatrix = translateX.split(', ');
+  const currentOffsetX = parseInt(transformMatrix[4]);
+  if (currentOffsetX < 0 && currentOffsetX >= -998) {
+    if (slideOffsetX === undefined) {
+      slideOffsetX = Math.abs(currentOffsetX);
+    }
+  
+    transformMatrix[4] = parseInt(transformMatrix[4]) + slideOffsetX;
+    console.log(transformMatrix.join(', '))
+  
+    goodsBlock.css('transform', transformMatrix.join(', '));
+  }
 })
