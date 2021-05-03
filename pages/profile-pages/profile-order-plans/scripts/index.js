53d04c3a61668f371ef6e0cc2bfce8ceef75101b
dmytro <tedi4t@gmail.com>
2021-05-03 17:56:22 +0300

    add unactive text on toggler

--- pages/profile-pages/profile-order-plans/scripts/index.js
+++ pages/profile-pages/profile-order-plans/scripts/index.js
@@ -1,5 +1,13 @@
 $('section#orders-1 button#state-toggler').click(function(e) {
   e.preventDefault();
+  const unactive = $(this)
+  .parent()
+  .parent()
+  .parent()
+  .hasClass('unactive');
+
+  $(this).html(!unactive ? 'لﺎﻌﻓ ﺮﯿﻏ' : 'لاعف')
+
   $(this)
     .parent()
     .parent()
