f6630fb025345e795f063fea2968f81236d866ff
dmytro <tedi4t@gmail.com>
2021-05-05 19:45:05 +0300

    add removal active from good

--- scripts/counter.js
+++ scripts/counter.js
@@ -15,7 +15,36 @@ $('.good-buy-btn-quantity-wrapper .good-buy-quantity').click(function() {
     .children('.good-buy-quantity.minus')
     .attr('disabled', (value === 1 && !addItem) ? true : false);
 
-  if (addItem || value !== 1) {
+  if (!addItem && value === 1) {
+    const goodElement = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+
+    const isGood = goodElement.attr('class').split(' ').includes('good');
+
+    if (isGood) {
+      $(goodElement).removeClass('active');
+
+      $(goodElement)
+        .children('.good-buy')
+        .children('.good-buy-btn-wrapper')
+        .css('display', 'block')
+
+      $(goodElement)
+        .children('.good-buy')
+        .children('.good-buy-btn-quantity-wrapper')
+        .css('display', 'none')
+
+      $(goodElement)
+        .children('.good-tick-active')
+        .css('display', 'none')
+
+      $('section#good-overlay-wrapper')
+        .css('display', 'none');
+    }
+  } else {
     valueItem.html(addItem ? value + 1 : value - 1);
   }
 })
